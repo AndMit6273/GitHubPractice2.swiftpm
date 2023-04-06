69001b1c5fcdaf465767e5b79f73b3925a6e452d
Andreas Mitilineos <amitilineos6273@student.sd25.org>
2023-04-06 14:19:29 -0500

    changed "other" to "noun"

--- ContentView.swift
+++ ContentView.swift
@@ -4,7 +4,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             NavigationView{
-                NavigationLink("To Other View") {
+                NavigationLink("To Noun View") {
                     NounView
                 }
             }
