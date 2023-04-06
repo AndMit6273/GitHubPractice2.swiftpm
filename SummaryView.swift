7d534f5971499216954cb7e6abdec17fcad2ab61
Andreas Mitilineos <amitilineos6273@student.sd25.org>
2023-04-06 14:33:28 -0500

    added Summary view

---
+++ SummaryView.swift
@@ -0,0 +1,14 @@
+//
+//  SummaryView.swift
+//  GitHubPractice2
+//
+//  Created by Andy Mitilineos on 4/6/23.
+//
+import SwiftUI
+import Foundation
+struct SummaryView: View {
+    var body: some View {
+        Text("I really like it when my (Adjective) (noun) goes to work and (verbs)")
+    }
+}
+
