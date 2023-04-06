a024505b1e7175a4cd211dd737943cc04b987a2d
Matthew Kao <mkao6187@stu.d214.org>
2023-04-06 14:22:01 -0500

    added NavigationLink

---
+++ NounView.swift
@@ -0,0 +1,14 @@
+//
+//  NounView.swift
+//  GitHubPractice2
+//
+//  Created by Matthew Kao on 4/6/23.
+//
+
+import Foundation
+
+struct NounView: View {
+    NavigationLink("Go to Verb") {
+        Verbview()
+    }
+}
