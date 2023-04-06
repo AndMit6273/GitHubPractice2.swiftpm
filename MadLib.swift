d6e9dc5e83a5e08985b1d8af0da88f5bcbdd5f8c
Matthew Kao <mkao6187@stu.d214.org>
2023-04-06 14:48:58 -0500

    added things

---
+++ MadLib.swift
@@ -0,0 +1,15 @@
+//
+//  MadLib.swift
+//  GitHubPractice2
+//
+//  Created by Matthew Kao on 4/6/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct MadLibApp {
+    var NounView:String
+    var VerbView:String
+    var AdjectiveView:String
+}
